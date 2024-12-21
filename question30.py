b94faa2ffc14ac2584466622159f036922529022
Rohit Sharma <rohtsharma777@gmail.com>
2024-12-21 20:11:47 +0545

    Delete question30.py

--- question30.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a program to calculate the delivery cost based on the weight of the
-package:
- If the package weighs less than 5kg, charge 5.
- If it weighs between 5kg and 20kg, charge 10.
- Check if the delivery is urgent; if yes, add $5 extra.
- If it weighs more than 20kg, charge 20.
-"""
