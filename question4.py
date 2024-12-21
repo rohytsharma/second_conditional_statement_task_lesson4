667c7b25d809e0e3644c683e4263f2c90403f1fb
Rohit Sharma <rohtsharma777@gmail.com>
2024-12-21 19:34:44 +0545

    Delete question4.py

--- question4.py
+++
@@ -1,8 +0,0 @@
-#Check whether the user input number is even or odd and display it to user.
-
-x=int(input("Enter any number: "))
-
-if x % 2 ==0:
-    print(f"the number {x} is even")
-else:
-    print(f"the number {x} is odd")
